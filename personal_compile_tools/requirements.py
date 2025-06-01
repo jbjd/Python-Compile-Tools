6aafaa23e4a7a02d82d6e719983a83245cf8a148
jbjd <jimdemetris@yahoo.com>
2025-06-01 11:13:42 -0500

    Mostly working

--- personal_compile_tools/requirements.py
+++ personal_compile_tools/requirements.py
@@ -7,15 +7,22 @@ from importlib.metadata import version as get_module_version
 
 _VALID_OPERATORS: list[str] = ["<", "<=", "!=", "==", ">=", ">", "~=", "==="]
 
-_VALID_OPERATOR_RE: str = f"(?:{'|'.join(_VALID_OPERATORS)})"
+_VALID_OPERATOR_RE: str = "|".join(_VALID_OPERATORS)
+
+_PEP440_RE_NO_CAP: str = (
+    r"[0-9]+(?:\.[0-9]+)*(?:(?:a|b|rc)[0-9]+)?(?:\.post[0-9]+)?(?:\.dev[0-9]+)?"
+)
 _PEP440_RE: str = (
-    r"[0-9]+(?:\.[0-9]+)*(?:(?:a|b|c|rc)[0-9]+)?(?:\.post[0-9]+)?(?:\.dev[0-9]+)?"
+    r"([0-9]+(?:\.[0-9]+)*)((?:a|b|rc)[0-9]+)?(\.post[0-9]+)?(\.dev[0-9]+)?"
 )
-_PEP440_WITH_OPERATOR_RE: str = _VALID_OPERATOR_RE + _PEP440_RE
+
+_PEP440_WITH_OPERATOR_RE: str = f"({_VALID_OPERATOR_RE})({_PEP440_RE_NO_CAP})"
 _REQUIREMENT_RE: str = (
-    r"^([A-Z0-9]|[A-Z0-9][A-Z0-9\._-]*[A-Z0-9])((" + _PEP440_WITH_OPERATOR_RE + r")+)$"
+    r"^([A-Z0-9]|[A-Z0-9][A-Z0-9\._-]*[A-Z0-9])(("
+    + f"(?:{_VALID_OPERATOR_RE})"
+    + _PEP440_RE_NO_CAP
+    + r")+)$"
 )
-_RULES_RE: str = f"^(?:({_PEP440_WITH_OPERATOR_RE})+)$"
 
 
 class VersionSegmentTypes(StrEnum):
@@ -42,23 +49,49 @@ class VersionSegment:
 class VersionRule:
     """Rule that a package installer must follow e.x. >=1.0.0"""
 
-    __slots__ = ("dev_version", "numeric_version", "operator", "post_release_version")
+    __slots__ = (
+        "dev_segment",
+        "operator",
+        "post_segment",
+        "pre_segment",
+        "raw_version",
+        "release_version",
+    )
 
     def __init__(self, operator: str, version: str) -> None:
         if operator not in _VALID_OPERATORS:
             raise ValueError(f"Invalid operator {operator}")
 
-        split_version: list[str] = version.split(".")
+        version = normalize_version(version)
+        test = re.search(_PEP440_RE, version)
 
-        for sub_version in version.split("."):
-            if not sub_version.isnumeric():
-                raise ValueError(f"Invalid version {version}")
+        if test is None:
+            raise ValueError(f"Invalid version {version}")
+
+        # TODO: Handle segments
+        release_version, pre_segment, post_segment, dev_segment = test.groups()
+
+        if release_version is None:
+            raise ValueError(f"Invalid version {version}")
 
         self.operator: str = operator
-        self.version: str = version
+        self.release_version: str = release_version
+        self.raw_version: str = version
+
+    def installed_version_is_compliant(self, module_name: str) -> bool:
+        """Returns True if version installed in this python interpreter
+        is compliant with the rule this object represents"""
+        installed_version: str = get_module_version(module_name)
+
+        # TODO: Handle all
+        match self.operator:
+            case "!=":
+                return installed_version != self.raw_version
+            case _:
+                return installed_version == self.raw_version
 
 
-class Requirements:
+class Requirement:
     """Represents a dependency of a python module"""
 
     __slots__ = ("name", "version_rules")
@@ -71,12 +104,15 @@ class Requirements:
         """Returns True when this dependency's version rules match the installed
         version in current python interpreter.
         Raises PackageNotFoundError if not present"""
-        installed_version: str = get_module_version(self.name)
+        return all(
+            version_rule.installed_version_is_compliant(self.name)
+            for version_rule in self.version_rules
+        )
 
 
 def parse_requirements_file(
     file_path: str, encoding: str = "utf-8"
-) -> list[Requirements]:
+) -> list[Requirement]:
     """Given a requirements file, returns a list of
     objects representing the dependencies"""
     with open(file_path, "r", encoding=encoding) as fp:
@@ -85,21 +121,27 @@ def parse_requirements_file(
     return parse_requirments(file_contents)
 
 
-def parse_requirments(requirements: str) -> list[Requirements]:
+def parse_requirments(raw_requirements: str) -> list[Requirement]:
     """Given contents of a requirements file, returns a list of
     objects representing the dependencies"""
 
+    requirements: list[Requirement] = []
+
     # regex slipt on \n without previous \
-    for line in re.split(r"(?<=[^\\])\s*\n", requirements):
+    raw_requirements = raw_requirements.strip()
+    for line in re.split(r"(?<=[^\\])\s*\n", raw_requirements):
         line = re.sub(r"\s+", " ", line)
 
         # For now, throw out env marker
         line = line.split(";")[0]
 
-    return []
+        requirement: Requirement = parse_requirement(line)
+        requirements.append(requirement)
+
+    return requirements
 
 
-def parse_requirement(requirement: str) -> Requirements:
+def parse_requirement(requirement: str) -> Requirement:
     """Given a single line of a requirements file, returns
     data parsed into a Requirements object"""
     requirement = re.sub(r"\s+", "", requirement)
@@ -110,19 +152,29 @@ def parse_requirement(requirement: str) -> Requirements:
         raise ValueError(f"Invalid requirment {requirement}")
 
     name: str = search_result.group(1)
-    version_rules: str = search_result.group(2)
+    version_rules_unparsed: str = search_result.group(2)
 
-    search_result = re.findall(_RULES_RE, version_rules)
+    split_version_rules_unparsed: list[tuple[str, str]] = re.findall(
+        _PEP440_WITH_OPERATOR_RE, version_rules_unparsed
+    )
 
-    return search_result
+    version_rules: list[VersionRule] = [
+        VersionRule(operator, version)
+        for operator, version in split_version_rules_unparsed
+    ]
+
+    return Requirement(name, version_rules)
 
 
 def version_is_pep440_compliant(version: str) -> bool:
     """Verifys version against pattern found here:
     https://peps.python.org/pep-0440/"""
 
-    return re.match(f"^{_PEP440_RE}$", version) is not None
+    return re.match(f"^{_PEP440_RE_NO_CAP}$", version) is not None
 
 
-test = parse_requirement("Asdg>=6.8.0<=7.0.0")
-print(test)
+def normalize_version(version: str) -> str:
+    """Normalizes version with rules found here:
+    https://peps.python.org/pep-0440/"""
+    # TODO: this is not complete
+    return version.replace("alpha", "a", 1).replace("beta", "b", 1)
--- tests/test_requirements.py
+++ tests/test_requirements.py
@@ -1,9 +1,25 @@
-from unittest.mock import patch
+import os
 
-from personal_compile_tools.requirements import Requirements
+from personal_compile_tools.requirements import Requirement, parse_requirements_file
+from tests.conftest import EXAMPLE_FOLDER
 
 _MODULE_NAME: str = "personal_compile_tools.requitements"
 
 
-def test_matches_installed_version():
-    pass
+def test_parse_requirements_file():
+    requirements: list[Requirement] = parse_requirements_file(
+        os.path.join(EXAMPLE_FOLDER, "requirements.txt")
+    )
+
+    assert len(requirements) == 1
+
+    requirement = requirements[0]
+
+    assert requirement.name == "some_module"
+    assert len(requirement.version_rules) == 2
+
+    assert requirement.version_rules[0].operator == ">="
+    assert requirement.version_rules[0].raw_version == "1.2.3"
+
+    assert requirement.version_rules[1].operator == "<="
+    assert requirement.version_rules[1].raw_version == "2.0.0"
