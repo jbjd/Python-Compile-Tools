39c51abaa15c9bfcceb9b819f0b36280d7de532a
jbjd <jimdemetris@yahoo.com>
2025-12-21 21:46:12 -0600

    Improvement: Add raise_if_not_root

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 
 import pytest
 
-from personal_compile_tools.validation import is_root
+from personal_compile_tools.validation import is_root, raise_if_not_root
 
 _MODULE_NAME: str = "personal_compile_tools.validation"
 
@@ -18,7 +18,7 @@ _is_root_cases: list = [
 
 @pytest.mark.parametrize("os_name,return_value,expected_is_root", _is_root_cases)
 def test_is_root(os_name: str, return_value: int, expected_is_root: bool):
-    """Should return correct bool given OS native response code"""
+    """Should return correct bool given OS native response code."""
 
     with (
         patch(f"{_MODULE_NAME}.os.name", os_name),
@@ -35,3 +35,17 @@ def test_is_root(os_name: str, return_value: int, expected_is_root: bool):
         ),
     ):
         assert is_root() is expected_is_root
+
+
+@pytest.mark.parametrize("is_root", (True, False))
+def test_raise_if_not_root(is_root: bool):
+    """Should raise PermissionsError if not root."""
+
+    error_message: str = "test"
+
+    with patch(f"{_MODULE_NAME}.is_root", return_value=is_root):
+        if not is_root:
+            with pytest.raises(PermissionError, match=error_message):
+                raise_if_not_root(error_message)
+        else:
+            raise_if_not_root(error_message)
